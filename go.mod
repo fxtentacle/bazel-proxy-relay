d2787903f2a0c61e93937753c731d6e00e463d85
Hajo Nils Krabbenhöft <hnk.git@hajo.me>
2022-03-13 18:52:16 +0100

    initial import

---
+++ go.mod
@@ -0,0 +1,9 @@
+module hajo.me/bazel-proxy-relay
+
+require (
+	github.com/elazarl/goproxy v0.0.0-20220115173737-adb46da277ac
+	github.com/spf13/cobra v1.1.3
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
+)
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"github.com/elazarl/goproxy"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+var (
+	Verbose                   bool
+	ListenServerPort          string
+	MitmCaCertificate         string
+	MitmCaKey                 string
+	RemoteProxyURL            string
+	SkipProxyCertificateCheck bool
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:  "proxygate",
+		Args: cobra.ExactArgs(0),
+		RunE: run,
+	}
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().StringVarP(&ListenServerPort, "listen", "l", "127.0.0.1:3128", "bind HTTP proxy to ip:port")
+	rootCmd.PersistentFlags().StringVarP(&MitmCaCertificate, "ca-cert", "c", "rootCA.pem", "CA certificate for proxying SSL")
+	rootCmd.PersistentFlags().StringVarP(&MitmCaKey, "ca-key", "k", "rootCA-key.pem", "CA private key for proxying SSL")
+	rootCmd.PersistentFlags().StringVarP(&RemoteProxyURL, "remote", "r", "https://user:pass@127.0.0.1/proxy/", "remote proxy URL prefix")
+	rootCmd.PersistentFlags().BoolVar(&SkipProxyCertificateCheck, "skip-proxy-certificate-check", false, "do not verify that remote proxy has a valid SSL certificate")
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	const explain = "Consider using https://github.com/FiloSottile/mkcert to create a trusted proxy CA."
+	ca_cert, err := ioutil.ReadFile(MitmCaCertificate)
+	if os.IsNotExist(err) {
+		log.Fatalf("Could not read CA certificate from %s. %s", MitmCaCertificate, explain)
+	}
+	ca_key, err := ioutil.ReadFile(MitmCaKey)
+	if os.IsNotExist(err) {
+		log.Fatalf("Could not read CA private key from %s. %s", MitmCaKey, explain)
+	}
+	goproxy.GoproxyCa, err = tls.X509KeyPair(ca_cert, ca_key)
+	if err != nil {
+		return err
+	}
+	if goproxy.GoproxyCa.Leaf, err = x509.ParseCertificate(goproxy.GoproxyCa.Certificate[0]); err != nil {
+		log.Fatalf("Error parsing CA: %v", err)
+	}
+	fmt.Println("Forwarding requests to", RemoteProxyURL)
+	httpproxy := goproxy.NewProxyHttpServer()
+	httpproxy.Verbose = Verbose
+	httpproxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
+	httpproxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		new_url := strings.Join([]string{RemoteProxyURL, req.URL.Scheme, req.URL.Hostname(), req.URL.Path}, "/")
+		if Verbose {
+			fmt.Println("REWRITE", req.URL, "=>", new_url)
+		}
+		var err error
+		req.URL, err = url.Parse(new_url)
+		if Verbose {
+			fmt.Println("PARSED INTO", req.URL, req.URL.Host, req.URL.User.Username(), req.URL.User.String())
+		}
+		if err != nil {
+			return req, goproxy.NewResponse(req, goproxy.ContentTypeText, 500, fmt.Sprintf("Error parsing \"%s\".", new_url))
+		}
+		req.Host = req.URL.Host
+		req.RequestURI = ""
+		if SkipProxyCertificateCheck {
+			http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			if Verbose {
+				fmt.Println("ERROR %v", err)
+			}
+			return req, goproxy.NewResponse(req, goproxy.ContentTypeText, 500, fmt.Sprintf("Error downloading from remote proxy: %v", err))
+		}
+		return req, resp
+	})
+	fmt.Println("Listening on", ListenServerPort)
+	fmt.Println("Configure Baze with:", fmt.Sprintf("HTTP_PROXY=%s HTTPS_PROXY=%s", ListenServerPort, ListenServerPort))
+	return http.ListenAndServe(ListenServerPort, httpproxy)
+}
